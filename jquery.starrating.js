62902bf784da71df26ec81624c723d1b78dceb7c
zanderwar <reece.alexander@gmail.com>
2018-01-31 22:13:27 +1000

    Added callback and readme

--- jquery.starrating.js
+++ jquery.starrating.js
@@ -2,31 +2,20 @@
     $.fn.starRating = function (options) {
         var settings = $.extend({
             stars: 5,
-            targetInput: false,
             current: 0,
-            callback: false
+            callback: function (value) {
+            }
         }, options);
 
         var $ul = this;
         var $li = $('<li class="star"><i class="fa fa-star"></i></li>');
 
         if (this.attr('data-current')) {
-            console.log('sup');
             settings.current = this.attr('data-current');
         }
 
         if (this.attr('data-stars')) {
             settings.stars = this.attr('data-current');
-        }
-
-        if (!settings.targetInput) {
-            if (this.attr('data-target')) {
-                settings.targetInput = this.attr('data-target');
-            } else {
-                settings.targetInput = '#' + makeId();
-                var $target = $('<input type="hidden"/>').attr('name', 'rating').attr('id', settings.targetInput);
-                $ul.after($target)
-            }
         }
 
         for (var i = 0; i < settings.stars; i++) {
@@ -52,16 +41,9 @@
             $(this).prevAll().removeClass('hover');
         });
 
-        $(settings.targetInput).val($ul.find('li.active').length);
+        var selected = $ul.find('li.active').length;
 
-        function makeId() {
-            var text = "";
-            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-            for (var i = 0; i < 8; i++)
-                text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-            return text;
-        }
+        $(settings.targetInput).val(selected);
+        settings.callback(selected);
     };
 }(jQuery));
